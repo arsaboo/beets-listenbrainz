54c0f0b1bb03b66bbe67909fff8e239d76341a59
Alok Saboo <arsaboo@gmx.com>
2023-12-17 13:57:09 -0500

    Update listenbrainz.py

--- beetsplug/listenbrainz.py
+++ beetsplug/listenbrainz.py
@@ -133,7 +133,7 @@ class ListenBrainzPlugin(BeetsPlugin):
 
     def get_weekly_playlist(self, index):
         """Returns a list of weekly playlists based on the index."""
-        playlists = self.get_listenbrainz_playlists(self.username)
+        playlists = self.get_listenbrainz_playlists()
         playlist = self.get_playlist(playlists[index].get("identifier"))
         return self.get_tracks_from_playlist(playlist)
 
